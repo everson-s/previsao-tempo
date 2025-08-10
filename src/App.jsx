eee1583c6a6b86359c96067fb71b1d21df2099a9
Everson dos Santos Pinheiro <79765901+everson-s@users.noreply.github.com>
2025-08-10 12:35:09 -0300

    Criando a url
    Criando uma url para conexão

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef()
+
+  async function searchCity(){
+    const city = inputRef.current.value
+    const key = "23f150ab5386b6c40024af907b7859f4"
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+
+    const ApiData = await axios.get(url)
+
+    console.log(city)
+  }
 
   return (
     <div>
       <h1>Previsao do tempo</h1>
-      <input type='text' placeholder='Digite o nome da cidade' />
-      <button>Buscar</button>
+      <input ref={inputRef} type='text' placeholder='Digite o nome da cidade' />
+      <button onClick={searchCity}>Buscar</button>
     </div>
   )
 }
